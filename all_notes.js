800b65d2407e34e1644206197125a3ee84040897
StanciuDragosIoan <stanciudragosioan92@gmail.com>
2025-05-15 19:07:13 +0300

    start rewriting all notes: covered preface

---
+++ all_notes.js
@@ -0,0 +1,53 @@
+/*
+
+    ### PREFACE 
+
+        NodeJS is a bit innovation that shits the paradigm in web development
+
+        NodeJS applications are written in JavaScript (the language of the web) 
+        so we can have a single application stack (share code between server and 
+        client). This also opens back-end possibilities for front-end engineers.
+
+        NodeJS contributes to the evolution of JavaScript as people realize they 
+        can use the language on the server. They also love its flexibility, 
+        pragmatism, event driven approach and hybrid nature (being in between 
+        OOP and functional).
+
+        NodeJS' single-threaded model and its asynchronous architecture beside 
+        advantages such as scalability and performance changed the way we approach
+        concurrency and parallelism. Mutexes are replaced by queues, threads by 
+        callbacks and synchronization by causality.
+
+        The last important aspect of Node.js lies in its package manager.
+
+        Node.js is different compared to other server-sie platforms. Although at 
+        first the way we can configure and structure our code seems confusing, but
+        the platform is now sufficiently mature to implement its own module system.
+
+        THIS BOOK COVERS:
+
+        The "Node way" 
+
+            How to approach Node.js development, how traditional design 
+            patterns look in Node.js
+
+        
+        Node.js patterns
+
+            A set of patterns to solve common Node.js design and coding problems:
+
+
+        How to write scalable and efficient Node.js applications:
+
+            Understanding the building blocks and principles of writing large 
+            and well-organized Node.js applications that can scale.
+            
+        
+        Code in "modern JavaScript":
+
+            JavaScript appeared in 1995 but it has evolved massively, this book 
+            uses its modern features. Throughout the book we will also see 
+            real-life libraries and technologies (which will be used to
+            demonstrate a pattern or technique).
+
+*/
---
+++ draft.js
@@ -0,0 +1,44 @@
+/*
+
+    ### CHAPTER 1: The Node.js Platform
+
+        Some principles define the developer experience. For Node.js its most peculiar
+        aspect is its asynchronous nature, which makes use of callbacks and promises.
+
+        Another important aspect of Node.js is its philosofy. It has its own culture and 
+        community and this influences how applications are created
+
+
+        The Node.js philosophy
+
+            All platforms have their own principles and guidelines. Some of these principles 
+            arise from the technology itself, some are enabled by the ecosystem and some are
+            trends in the community, while others are borrowed from other platforms.In Node.js
+            some of these principles come directly from its creator Ryan Dahl, while others come 
+            from the people who controbute to the core.
+
+       
+        Small core
+
+            The Node.js core (the runtime and built in module) follows the principle of having the 
+            smallest set of functionalities, while the rest of the modules in the ecosystem live outside
+            the core. This allows the community to experiment and iterate faster, while also being
+            easely maintainable.
+
+
+        Small modules
+
+            Node.js uses modules as the fundamental building block for creating applications
+            while structuring code to be reusable. This principle has its roots in the Unix 
+            philosophy, and particularly in two of its precepts:
+"Small is beautiful."
+"Make each program do one thing well."
+
+        Node.js uses the concept of a module as the fundamental means for structuring the
+code of a program. It is the building block for creating applications and reusable
+libraries. In Node.js, one of the most evangelized principles is designing small
+modules (and packa
+
+        
+
+*/
